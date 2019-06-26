c63360ff4ebf8066bc1adc0f23b87964bd4e4c96
Lukasz Choragwicki <choragwicki.lukasz@gmail.com>
2019-06-26 16:22:20 +0200

    add skills-services

--- src/app/components/examples/skills/skills.component.ts
+++ src/app/components/examples/skills/skills.component.ts
@@ -1,15 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-
+import { SkillsServiceService } from 'src/app/services/skills-service.service';
 @Component({
-  selector: 'app-skills',
-  templateUrl: './skills.component.html',
-  styleUrls: ['./skills.component.scss']
+	selector: 'app-skills',
+	templateUrl: './skills.component.html',
+	styleUrls: [ './skills.component.scss' ]
 })
 export class SkillsComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+	constructor(public skillsServiceService: SkillsServiceService) {}
+	ngOnInit() {}
 }
