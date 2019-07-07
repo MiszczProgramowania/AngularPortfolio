501502ab2ed1a16195cf08ded7ac59a50f2d5d6a
Lukasz Choragwicki <choragwicki.lukasz@gmail.com>
2019-07-07 19:51:30 +0200

    add photo

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,43 +5,43 @@
 export const environment = {
 	skills: [
 		{
-			imgSrc: 'assets/images/html5.png',
+			imgSrc: './assets/images/html5.png',
 			title: 'HTML',
 			description: 'Stosuję semantyczny kod zgodnie ze standardami W3C'
 		},
 		{
-			imgSrc: 'assets/images/css3.png',
+			imgSrc: './assets/images/css3.png',
 			title: 'CSS',
 			description: 'Kontroluję wygląd oraz układ elementów na stronie internetowej.'
 		},
 		{
-			imgSrc: 'assets/images/Sass.png',
+			imgSrc: './assets/images/Sass.png',
 			title: 'SASS',
 			description: 'Wykorzystuję składnię do szybszego pisania kodu'
 		},
 		{
-			imgSrc: 'assets/images/bootstrap.png',
+			imgSrc: './assets/images/bootstrap.png',
 			title: 'Bootstrap',
 			description:
 				'Wykorzystuję gotowe rozwiązania CSS i JS do przyspieszania pracy przy tworzeniu strony internetowej.'
 		},
 		{
-			imgSrc: 'assets/images/rwd.png',
+			imgSrc: './assets/images/rwd.png',
 			title: 'RWD',
 			description: 'Używam media queries oraz flexboxa do tworzenia responsywnych stron.'
 		},
 		{
-			imgSrc: 'assets/images/git.png',
+			imgSrc: './assets/images/git.png',
 			title: 'GIT',
 			description: 'To narzędzie pozwala mi na kontrolowanie wprowadzania zmian w projekcie.'
 		},
 		{
-			imgSrc: 'assets/images/javascript.png',
+			imgSrc: './assets/images/javascript.png',
 			title: 'JavaScript',
 			description: 'Znam podstawy.'
 		},
 		{
-			imgSrc: 'assets/images/angular.png',
+			imgSrc: './assets/images/angular.png',
 			title: 'Angular',
 			description: 'Znam podstawy'
 		}
